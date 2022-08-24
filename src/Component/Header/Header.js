c46a44d6f4c4fa53cbbc0b4c08c85c0997dc7aa6
SumiAkter2 <akteralisha36@gmail.com>
2022-08-24 08:30:03 +0600

    simple header

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="flex justify-between items-center p-4">
+      <div className="flex  items-center ">
+        <img
+          style={{ width: "50px" }}
+          src="https://i.ibb.co/Z1LRDsZ/sharepal-logo-removebg-preview.png"
+          alt="sharePal-logo"
+        />
+        <h1 className="text-3xl pl-2">SharePal</h1>
+      </div>
+      <div>
+        <button className="btn btn-outline btn-info  pr-4">LogIn/SignIn</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
